fe883ae273fb3600833c8c11b0566ada68f30935
Stefano Gajon <stefanogajon@Stefanos-MBP.homenet.telecomitalia.it>
2017-04-24 07:22:31 +0200

    modified comment

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import {Observable, Observer} from "rxjs";
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 ////////////////////////////////////////////
-//simple observer implementation with from 
+//simple observerable with from 
 ////////////////////////////////////////////
 let source1 = Observable.from(numbers);
 
@@ -25,7 +25,7 @@ class MyObserver implements Observer<number>{
 source1.subscribe(new MyObserver());
 
 ////////////////////////////////////////////
-//simple observer implementation with create
+//simple async observable with create
 ////////////////////////////////////////////
 
 let source2 = Observable.create(observer => {
