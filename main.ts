d65bc41cdd80f0c049ad1dc14b33defd266a19b2
Stefano Gajon <stefanogajon@Stefanos-MBP.homenet.telecomitalia.it>
2017-04-24 07:13:02 +0200

    observable create first example

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
-import {Observable} from "rxjs";
+import {Observable, Observer} from "rxjs";
 
-let numbers = [1, 2, 3];
-let source = Observable.from(numbers);
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+////////////////////////////////////////////
+//simple observer implementation with from 
+////////////////////////////////////////////
+let source1 = Observable.from(numbers);
 
-class MyObserver{
+class MyObserver implements Observer<number>{
     
     next(val){
         console.log(`value: ${val}`);
@@ -19,4 +22,31 @@ class MyObserver{
     }
 }
 
-source.subscribe(new MyObserver());
+source1.subscribe(new MyObserver());
+
+////////////////////////////////////////////
+//simple observer implementation with create
+////////////////////////////////////////////
+
+let source2 = Observable.create(observer => {
+    
+    let index = 0;
+    let produceValue = () => {
+        observer.next[numbers[index++]]
+    }
+
+    if (index < numbers.length){
+        setTimeout(produceValue, 500)
+    } 
+    else{
+        observer.complete();
+    }
+
+    produceValue();
+});
+
+source2.subscribe(
+    val => {console.log(`value: ${val}`);},
+    err => {console.log(`error: ${err}`);},
+    () => {console.log(`finished`);}
+);
