c98f2e76f48d12b4c43ca9ca72644afa16531658
Stefano Gajon <stefanogajon@Stefanos-MBP.homenet.telecomitalia.it>
2017-04-28 06:17:03 +0200

    implementing a retry strategy

--- main-simple-ajax-flatmap.ts
+++ main-simple-ajax-flatmap.ts
@@ -10,15 +10,32 @@ function load(url:string){
         let xhr = new XMLHttpRequest();
 
         xhr.addEventListener("load", () => {
-            let data = JSON.parse(xhr.responseText);
-            observer.next(data);
-            observer.complete();
+            if (xhr.status === 200){
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            }
+            else{
+                observer.error(xhr.statusText);
+            }
         });
 
         xhr.open("GET", url);
         
         xhr.send();
-    });
+    }).retryWhen(retryStrategy({ attempts: 3, delay: 1000 }));
+}
+
+function retryStrategy({ attempts = 4, delay = 500 }){ //default constructor
+    return function(errors){
+        return errors
+            .scan((acc, val) => {
+                console.log(acc, val);
+                return ++acc;
+            }, 0)
+            .takeWhile(acc => acc < attempts)
+            .delay(delay);
+    }
 }
 
 function renderMovies(movies){
